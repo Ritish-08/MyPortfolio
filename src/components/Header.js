da8503c88ea7208357f2b4b26a02d65f5c1edb06
Ritish <rchoudhary_be20@thapar.edu>
2023-08-06 23:48:53 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <a href='#'>
-           <button><h1 className='first'>Ritish Choudhary</h1></button>
+           <button><h1 className='first'>Ritish <br /> Choudhary</h1></button>
           </a>
           <button className="btn btn-sm">Work With Me</button>
         </div>
